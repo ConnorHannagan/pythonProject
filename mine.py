84cd3a45ea76c25d8d34ed4008f88915fa10ba87
ConnorHannagan <101076314+ConnorHannagan@users.noreply.github.com>
2022-03-25 10:40:12 +1300

    Added prints

--- mine.py
+++ mine.py
@@ -1,20 +1,21 @@
+import random
 class Card:
     def __init__(self, value, suit):
+        self.known = False
         self.value = value
         self.suit = suit
-        if self.suit == "S" or self.suit == "C":
+        if self.suit == "S" or "C":
             self.colour = "black"
-        elif self.suit == "H" or self.suit == "D":
+        elif self.suit == "H" or "D":
             self.colour = "red"
-
-
-
-    def getval(self):
+    def val(self):
         return self.value
-    def getsuit(self):
+    def suit(self):
         return self.suit
-    def getcolour(self):
+    def colour(self):
         return self.colour
+    def known(self):
+        return self.known
 
 class Deck:
     def __init__(self):
@@ -23,11 +24,30 @@ class Deck:
         for a in ("D", "H", "C", "S"):
             for i in range(13):
                 self.cards.append(Card(i+1, a))
+    def shuffle(self):
+        random.shuffle(self.cards)
+    def getcard(self, position):
+        if position == 1:
+            return position.self.cards
+        if position > 1:
+            if position.self.cards.known == False:
+                return False
+            else:
+                return position.self.cards
 
     def print(self):
         for i in self.cards:
-            print(i.getval(), i.getsuit(),i.getcolour())
+            print(i.value(), i.suit(), i.colour())
 
-game = Deck()
-game.create()
-game.print()
+
+drawdeck = Deck()
+drawdeck.create()
+discardeck = Deck()
+board = []
+for i in range(7):
+    board.append(Deck())
+for i in range(7):
+    for b in range(7):
+
+
+print(drawdeck.getcard(1).val)
