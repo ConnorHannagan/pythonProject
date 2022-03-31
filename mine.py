6196fd3ef8d2a600cc2e2251684950ee63281e7f
ConnorHannagan <101076314+ConnorHannagan@users.noreply.github.com>
2022-03-31 14:23:40 +1300

    Added prints

--- mine.py
+++ mine.py
@@ -1,41 +1,59 @@
 import random
 class Card:
-    def __init__(self, value, suit):
+
+    def __init__(self, Value, Suit):
         self.known = False
-        self.value = value
-        self.suit = suit
-        if self.suit == "S" or "C":
+        self.value = Value
+        self.suit = Suit
+        if self.suit == "S" or self.suit == "C":
             self.colour = "black"
-        elif self.suit == "H" or "D":
+        elif self.suit == "H" or self.suit ==  "D":
             self.colour = "red"
+
+
     def val(self):
         return self.value
-    def suit(self):
+
+    def suite(self):
         return self.suit
+
     def colour(self):
         return self.colour
+
     def known(self):
         return self.known
 
+    def flip(self):
+        self.known = True
+
+
 class Deck:
+
     def __init__(self):
         self.cards = []
+
+
     def create(self):
         for a in ("D", "H", "C", "S"):
             for i in range(13):
                 self.cards.append(Card(i+1, a))
+
     def shuffle(self):
         random.shuffle(self.cards)
+
     def veiwcard(self,position):
         if position > len(self.cards)-1:
-            return "       "
+            return "        "
+        elif self.cards[position].known == True:
+                temp = self.cards[position]
+
+                if temp.val() < 10:
+                    temp = " [ " + temp.suite() + str(temp.val()) + "  ]"
+                else:
+                    temp = " [ " + temp.suite() + str(temp.val()) + " ]"
+                return temp
         else:
-            temp = self.cards[position]
-            if temp.val() < 10:
-                temp = " [ " + str(temp.suit()) + str(temp.val()) + "  ]"
-            else:
-                temp = " [ " + str(temp.suit()) + str(temp.val()) + " ]"
-            return temp
+            return " [     ]"
 
     def getcard(self, position):
         if position > len(self.cards):
@@ -51,6 +69,7 @@ class Deck:
         #         return False
         #     else:
         #         return position.self.cards
+
     def add(self, card):
         self.cards.append(card)
         return()
@@ -62,10 +81,25 @@ class Deck:
     def getl(self):
         return len(self.cards)
 
+    def flip(self):
+        self.cards[len(self.cards)-1].flip()
+
+
+def displayboard():
+    for i  in range(7):
+        print(board[0].veiwcard(i),board[1].veiwcard(i),board[2].veiwcard(i),board[3].veiwcard(i),board[4].veiwcard(i),board[5].veiwcard(i), board[6].veiwcard(i))
+
+def fliptop():
+    for i in range(7):
+        board[i].flip()
+
+
+create board
 drawdeck = Deck()
 drawdeck.create()
 discardeck = Deck()
 board = []
+drawdeck.shuffle()
 for i in range(7):
     board.append(Deck())
 for i in range(7):
@@ -74,20 +108,5 @@ for i in range(7):
             board[b].add(drawdeck.getcard(0))
 
 
-for i in range(7):
-    print(board[0].veiwcard(i),board[1].veiwcard(i),board[2].veiwcard(i),board[3].veiwcard(i),board[4].veiwcard(i),board[5].veiwcard(i), board[6].veiwcard(i))
-
-
-
-
-# for i in range(7):
-#     row = []
-#     for a in range(7):
-#         temp = board[a].getcard(i-1)
-#         if temp == " ":
-#             row.append("")
-#         else:
-#             row.append(temp.val())
-#     print(row)
 
 
