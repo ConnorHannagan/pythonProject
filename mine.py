34d756cc7077a1de6c2abde52d704b94f307e8fc
ConnorHannagan <101076314+ConnorHannagan@users.noreply.github.com>
2022-04-15 04:12:46 +1200

    Added prints

--- mine.py
+++ mine.py
@@ -69,30 +69,38 @@ class Deck:
 
 #returns a card in a printatble form
     def veiwcard(self, position):
-        normal = "\033[0;37;40m"
+        normal = "\033[0;37;40m" # =varible to make it easier when we want to go back to normal print colour
+
         if position <= 0:
-            if len(self.cards) == 0:
+            if len(self.cards) == 0: #if the deck is empty it returns a "blank car" to show there is a deck there but no card
                 temp = "[ " + self.basestate + "  ]"
                 return temp
 
+        # if a poistion that is out of the upper bouds of our cards array, it will return a blank space
         if position > len(self.cards) - 1:
             return normal + "       "
 
+        #returns printable card if value is known
         elif self.cards[position].known == True:
             temp = self.cards[position]
+
+        # adds a space to make 9 and 10 the same length
             if temp.val(True) != 10:
                 temp = temp.pcolour() +"[ " + temp.suite() + str(temp.val(False)) + "  ]"+ normal
             else:
                 temp = temp.pcolour() + "[ " + temp.suite() + str(temp.val(False)) + " ]" +normal
             return temp
+
+        #The card is unkown it will return a back of a card
         else:
             return normal + "\033[0;27;44m"+"[ xxx ]"+normal
 
 #returns a card class and removing it from the deck
     def getcard(self, position):
-        if position > len(self.cards):
+
+        if position > len(self.cards): #checks the position is within the array, if not it returns nothing
             return " "
-        else:
+        else: #if positinon is within the array of cards, it returns the card in that position then removes from cards array
             temp = self.cards[position]
             self.cards.pop(position)
             return temp
@@ -101,19 +109,23 @@ class Deck:
     def get(self, position):
         return self.cards[position]
 
+#adds a card to the cards array
     def add(self, card):
         self.cards.append(card)
         return ()
 
+#returns the legnth of the cards array
     def getl(self):
         return len(self.cards)
 
+#tells the top card of the deck to make its known value True
     def flip(self):
         self.cards[len(self.cards) - 1].flip()
 
-
+#displays board
 def displayboard():
-    normal = "\033[0;37;40m"
+    normal = "\033[0;37;40m" #sets normal print colour
+
     # printing off foundation numbering
     for i in range(28):
         print(" ", end='')
@@ -145,13 +157,13 @@ def displayboard():
             print("",board[a].veiwcard(i), end=' ')
         print(" ", i + 1)  # right side numbers
 
-
+#tells all the decks in the board array to change to top card's known value to true
 def fliptop():
     for i in range(7):
         if board[i].getl() > 0:
             board[i].flip()
 
-
+#creates the array of decks it uses and sets the cards up in the correct order
 def createboard():
     for i in range(7):  # populates the Board array with our decks
         board.append(Deck())
@@ -162,14 +174,12 @@ def createboard():
         for b in range(7):  # populates the card
             if b >= i:
                 board[b].add(drawdeck.getcard(0))
-    for i in ("♦", "♥", "♣", "♠"):
+    for i in ("♦", "♥", "♣", "♠"): # creates foundations
         foundations.append(Deck())
 
-
+#checks two cards to see if putting one on top another is a legal move
 def legal(first, placing):
     if first.colour != placing.colour:
-        print(first.val(True))
-        print(placing.val(True) + 1)
         if first.val(True) == placing.val(True) + 1:
             return True
         else:
@@ -186,7 +196,7 @@ def addfoundation(card):
         for i in range(len(foundations)):  # checks all the foundations
             if foundations[i - 1].getl() > 0:  # sees if they are populated
                 if foundations[i - 1].get(foundations[i - 1].getl()-1).suite() == card.suite():  # if populated checks to see if its the same suite
-                    if foundations[i - 1].get(foundations[i-1].getl()-1).val(True)+1 == card.val(True):
+                    if foundations[i - 1].get(foundations[i-1].getl()-1).val(True)+1 == card.val(True):# checks if the card is the a value above the card alreay in the foundation deck
                         foundations[i - 1].add(card)
                         return True
         return False
@@ -196,8 +206,10 @@ def addfoundation(card):
 def getinput():
     userinpt = input("\nplease select the pile you want to pick up from (A,B,C,D,E,F,G)\nor entre \n1) Draw\n2) Pickup from draw pile\n3) Pick up from foundations\n").upper()
     a = 0
+
+    # draws a card from the draw deck to the discard deck
     if userinpt == "1":
-        if drawdeck.getl() == 0:
+        if drawdeck.getl() == 0: #if there are no cards if the draw deck it takes all the cards from the discard deck and puts it into the draw deck
             for i in range(discardeck.getl()):
                 discardeck.get(0).hide()
                 drawdeck.add(discardeck.getcard(0))
@@ -206,9 +218,10 @@ def getinput():
         discardeck.flip()
         return
 
+    #takes a card from the top of the discard deck
     if userinpt == "2":
 
-        if discardeck.getl() == 0:
+        if discardeck.getl() == 0: #returns an error if there is nothing in the discardeck
             clear()
             error("enter to continue")
             return
@@ -217,33 +230,34 @@ def getinput():
 
         userinpt = input("\nselect the pile you want to place it on (A,B,C,D,E,F,G)\nor\n1) Place into foundations\n").upper()
         a = 0
-        if userinpt == "1":
+        if userinpt == "1": # if the user wants to put it into the foundation decks
             if addfoundation(discardeck.get(discardeck.getl() - 1)) == True:
                 discardeck.getcard(discardeck.getl() - 1)
                 return
         else:
-            for i in ("A", "B", "C", "D", "E", "F", "G"):
+            for i in ("A", "B", "C", "D", "E", "F", "G"):# it the user want to put the card onto the board
                 a += 1
                 if userinpt == i:
                     z = a
                     a = True
                     break
             if a != True:
-                error("please input a valid input 1")
+                error("please input a valid input")
                 return
 
 
-            if board[z-1].getl() == 0:
+            if board[z-1].getl() == 0: # if the deck in the board refrenced is empty, it makes sure that a king will populate it first
                 if discardeck.get(discardeck.getl()-1).val(True) == 13:
                     board[z-1].add(discardeck.getcard(discardeck.getl() - 1))
                     return
-            elif legal(board[z-1].get(board[z-1].getl()-1), discardeck.get(discardeck.getl() - 1)) == True:
+            elif legal(board[z-1].get(board[z-1].getl()-1), discardeck.get(discardeck.getl() - 1)) == True: #checks if the move is legal
                 board[z-1].add(discardeck.getcard(discardeck.getl() - 1))
                 return
             else:
                 error("not a legal move")
                 return
 
+#takes a card from the founation decks
     if userinpt == "3":
         userinpt = input("what foundation do you want to get a card from, 1,2,3,4")
         x = 0
